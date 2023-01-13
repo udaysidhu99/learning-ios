178b0801ed638b62400ce71eccd3326043d7f944
Uday <udaysidhu1@outlook.com>
2023-01-13 22:19:58 +0530

    Initial Commit

---
+++ learning_github/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  learning_github
+//
+//  Created by Uday Sidhu on 13/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ learning_github/learning_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_githubApp.swift
+//  learning_github
+//
+//  Created by Uday Sidhu on 13/01/23.
+//
+
+import SwiftUI
+
+@main
+struct learning_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
